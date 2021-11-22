855ba3ef7ba3893be9238089fbea1d97468bcf96
liuyahua <liuyahua@haomo-studio.com>
2021-11-22 21:53:50 +0800

    feature:css样式文本两端对齐

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@
 import Home from "@/pages/test-code/home.vue";
 
 import RegTest1113 from '@/pages/reg-test/reg-test1113.vue'
+import CssTest from '@/pages/reg-test/css-test.vue'
+
 
 const routes = [
   {
@@ -20,18 +22,26 @@ const routes = [
     ],
   },
   {
-    id: 20,
+    id: 50,
     text: "正则",
     icon: "el-icon-location",
     arr: [
       {
-        id: 2001,
+        id: 5001,
         meta: {
           title: "正则匹配",
         },
         path: "/regTest1113",
         component: RegTest1113,
       },
+      {
+        id:5002,
+        meta:{
+          title:'样式'
+        },
+        path:'/css-test',
+        component:CssTest,
+      }
     ],
   }
 ];
---
+++ src/pages/reg-test/css-test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+      <li>大道至简</li>
+      <li>菩提</li>
+      <li>只可意会</li>
+      <li>不可言</li>
+    </ul>
+
+    <div class='box'></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="css" scoped>
+ul{
+  float: left;
+}
+ul li{
+  width:80px;
+  height:30px;
+  list-style-type: none;
+  background: orange;
+  line-height: 30px;
+  margin:5px 0;
+  padding:0 10px;
+  box-sizing: border-box;
+  text-align-last: justify;
+}
+
+/* .box{
+  width:200px;
+  height:200px;
+  background: orange;
+  resize: both;
+  
+} */
+</style>
