35e9788529f8190afc81c7c7a78d001f0c1edc22
liuyahua <liuyahua@haomo-studio.com>
2021-11-30 19:56:36 +0800

    fix:增加map方法

--- src/pages/reg-test/css-test.vue
+++ src/pages/reg-test/css-test.vue
@@ -7,28 +7,60 @@
       <li>不可言</li>
     </ul>
 
-    <div class='box'></div>
+    <div class="box"></div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  mounted() {
+    const array = [1, 2, 3, 4, 5, 6];
+    array.sort(function () {
+      return Math.random() - 0.5;
+    });
+    console.log('array', array);
+
+    // function isNum(n) {
+    //   return !isNaN(parseInt(n)) && isFinite(n);
+    // }
+
+    function isStr(value) {
+      return typeof value === 'string';
+    }
+
+    console.log(isStr(3));
+
+    function isNull(value) {
+      return value === null || value === undefined;
+    }
+    console.log(isNull(123));
+
+    // map() 方法对数组每一项运行给定函数，返回每次函数调用的结果组成的数组
+    let users = [
+      { name: 't1', email: 't1@163.com' },
+      { name: 't2', email: 't2@163.com' },
+      { name: 't3', email: 't3@163.com' },
+    ];
+    let userRes = users.map(ele => {
+      return ele.email;
+    });
+    console.log('userRes', userRes);
+  },
+};
 </script>
 
 <style lang="css" scoped>
-ul{
+ul {
   float: left;
 }
-ul li{
-  width:80px;
-  height:30px;
+ul li {
+  width: 80px;
+  height: 30px;
   list-style-type: none;
   background: orange;
   line-height: 30px;
-  margin:5px 0;
-  padding:0 10px;
+  margin: 5px 0;
+  padding: 0 10px;
   box-sizing: border-box;
   text-align-last: justify;
 }
