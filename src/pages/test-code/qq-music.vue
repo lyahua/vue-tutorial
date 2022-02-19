34b2f8a450a0e5926d501a31b4800dfe1d6a8f97
liuyahua <1251271810@qq.com>
2022-02-19 17:34:25 +0800

    fix: 深复制使页面同步变化

--- src/pages/test-code/qq-music.vue
+++ src/pages/test-code/qq-music.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="music-page">
     <div class="search-content">
       <a-input-search
         placeholder="input search text"
@@ -12,10 +12,29 @@
         <span>{{ item.singer }}</span>
       </div>
     </div>
+    <div class="cache-content">
+      <div class="title-content">
+        <div
+          v-for="item in songerName"
+          :key="item.id"
+          class="songer-name"
+          @click="searchSong(item.name)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+      <div class="content">
+        <div v-for="item in cacheMusicList[activeName]" :key="item.id">
+          <span>{{ item.name }}</span>
+          <span>{{ item.singer }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { message } from "ant-design-vue";
 // import { getNewQqMusic, getKgMusic } from "@/utils/api";
 import { mapActions, mapState } from "vuex";
 export default {
@@ -23,25 +42,20 @@ export default {
   data() {
     return {
       //   musicList: [],
+      songerName: [
+        { id: 1, name: "王菲" },
+        { id: 2, name: "王力宏" },
+        { id: 3, name: "小猫" },
+        { id: 4, name: "王狗" },
+      ],
+      activeName: "",
     };
   },
   created() {
-    // let kgstr =
-    //   "callback=callback123&keyword=%E7%8E%8B&page=1&pagesize=30&bitrate=0&isfuzzy=0&tag=em&inputtype=0&platform=WebFilter&userid=0&clientver=2000&iscorrection=1&privilege_filter=0&token=&srcappid=2919&clienttime=1645233911726&mid=1645233911726&uuid=1645233911726&dfid=-&signature=51E9A39538A5C1D5F01E073EFC3FA575";
-
-    // let params2 = {};
-    // kgstr.split("&").map((ele) => {
-    //   let arr = ele.split("=");
-    //   params2[arr[0]] = arr[1];
-    // });
-    // params2.keyword = decodeURI(params2.keyword);
-    // getKgMusic(params2).then((res) => {
-    //   console.log("res酷狗", res);
-    // });
     console.log("this.$stroe", this.$store);
   },
   methods: {
-    ...mapActions("music", ["getQqMusicInterface"]), // 接口
+    ...mapActions("music", ["getQqMusicInterface", "getCacheQqMusicInterface"]), // 接口
     onSearch(val) {
       let str =
         "_=1645232392501&cv=4747474&ct=24&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=1&uin=1251271810&g_tk_new_20200303=2085573607&g_tk=2085573607&hostUin=0&is_xml=0&key=%E5%91%A8%E6%9D%B0";
@@ -56,17 +70,75 @@ export default {
       //     console.log("res", res);
       //     this.musicList = res.data.data.song.itemlist;
       //   });
-      this.getQqMusicInterface(params);
+      if (params.key?.trim()) {
+        this.getQqMusicInterface(params);
+      } else {
+        message.warning("请输入要搜索的文字");
+      }
+    },
+    // 点击名字搜索音乐
+    searchSong(name) {
+      //   console.log("name", name);
+      let str =
+        "_=1645232392501&cv=4747474&ct=24&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=1&uin=1251271810&g_tk_new_20200303=2085573607&g_tk=2085573607&hostUin=0&is_xml=0&key=%E5%91%A8%E6%9D%B0";
+      let params = {};
+      str.split("&").map((ele) => {
+        let arr = ele.split("=");
+        params[arr[0]] = arr[1];
+      });
+      params.key = name;
+      this.activeName = name;
+      console.log("params", params);
+      let lst = this.cacheMusicList[name];
+      if (!(lst && lst.length > 0)) {
+        this.getCacheQqMusicInterface(params);
+      }
     },
   },
   computed: {
-    ...mapState("music", ["musicList"]),
+    ...mapState("music", ["musicList", "cacheMusicList"]),
   },
 };
 </script>
 
 <style lang="css" scoped>
+.music-page {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #fff;
+  display: flex;
+}
 .search-content {
   width: 200px;
+  height: 400px;
+  background: #ccc;
+  /* float: left; */
+}
+.cache-content {
+  width: 400px;
+  height: 400px;
+  background: red;
+  display: flex;
+}
+.title-content {
+  width: 60px;
+  height: 400px;
+  background: greenyellow;
+}
+.content {
+  width: 340px;
+  height: 400px;
+  background: #ccc;
+}
+.songer-name {
+  line-height: 42px;
+  background: orange;
+  text-align-last: justify;
+}
+.songer-name:nth-child(2n) {
+  background: goldenrod;
 }
 </style>
--- src/store/modules/music.js
+++ src/store/modules/music.js
@@ -5,11 +5,17 @@ export default {
   state: {
     str: "hello world",
     musicList: [],
+    cacheMusicList: {}, // 测试缓存的musiclist
   },
   getters: {},
   mutations: {
     updateMusicList(state, payload) {
       state.musicList = payload;
+    },
+    // 根据条件更新缓存music，有则用，无则更新
+    updateCacheMusicList(state, payload) {
+      state.cacheMusicList[payload.k] = payload.v;
+      state.cacheMusicList=JSON.parse(JSON.stringify(state.cacheMusicList))
     },
   },
   actions: {
@@ -17,6 +23,14 @@ export default {
       getNewQqMusic(payload).then((res) => {
         // console.log('res store',res)
         store.commit("updateMusicList", res.data.data.song.itemlist);
+      });
+    },
+    // 测试缓存
+    getCacheQqMusicInterface(store, params) {
+      getNewQqMusic(params).then((res) => {
+        // console.log("payload", payload, "rs", res);
+        let payload = { k: params.key, v: res.data.data.song.itemlist };
+        store.commit("updateCacheMusicList", payload);
       });
     },
   },
